cca55b03bc59ffcc0697a37dd679d108d537397c
Ahmed Zaghdoudi <ahmed.zaghdoudi@esprit.tn>
2022-02-16 00:16:51 +0100

    main

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Accueil from "./components/accueil";
+import Timeline from "./components/timeline/time-line";
+import Timelinefriends from "./components/timeline/timeline-friends";
+import Timelinegroups from "./components/timeline/timeline-groups";
+import Timelinephotos from "./components/timeline/timeline-photos";
+import Timelinevideos from "./components/timeline/timeline-videos";
+import About from "./components/timeline/about";
+import Messages from "./components/timeline/messages";
+import Indexcompany from "./components/company/index-company";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Accueil />} />
+        <Route path="/timeline" element={<Timeline />} />
+        <Route path="/timelinefriends" element={<Timelinefriends />} />
+        <Route path="/timelinegroups" element={<Timelinegroups />} />
+        <Route path="/timelinephotos" element={<Timelinephotos />} />
+        <Route path="/timelinevideos" element={<Timelinevideos />} />
+
+        <Route path="/company" element={<Indexcompany />} />
+        <Route path="/messages" element={<Messages />} />
+      </Routes>
+    </BrowserRouter>
+    // </div>
+    // <Timeline />
+    // <Timelinegroups />
+    // <Timelinephotos />
+    // <Timelinevideos />
+    // <Messages />
+    //<About />
   );
 }
 
