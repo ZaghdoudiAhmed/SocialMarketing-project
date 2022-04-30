5407f9ed5a43e8c6faadbde1f3c7bb1c8fa74934
rimabensalah <ryma.bensalah@esprit.tn>
2022-04-30 22:07:33 +0100

    orderversion2
    orderversion

--- server/models/comment.js
+++ server/models/comment.js
@@ -1,19 +1,32 @@
 const mongoose = require("mongoose");
-const { Schema } = mongoose;
-const reply = require('../models/reply')
-const blog = require('../models/Blog')
-const user = require('../models/User')
-const comment = mongoose.Schema({
-    _id: Schema.Types.ObjectId,
-    subject : String,
-  desciption: String,
-  reply :[ { type: Schema.Types.ObjectId, ref: 'reply' }],
-  blog :{ type: Schema.Types.ObjectId, ref: 'blog' },
-  publisher :{ type: Schema.Types.ObjectId, ref: 'user' },
+
+var Schema = mongoose.Schema;
+
+var Comment = new Schema({
+  Body: String,
+  Date_creation: { type: Date, default: Date.now },
+  Likes: { type: Number, default: 0 },
+  Dislikes: { type: Number, default: 0 },
+  Creator: { type: Schema.Types.ObjectId, ref: "users" },
+  Post_id: { type: Schema.Types.ObjectId, ref: "Post" },
+  comments: [
+    {
+      Body: String,
+      Date_creation: { type: Date, default: Date.now },
+      Likes: { type: Number, default: 0 },
+      Dislikes: { type: Number, default: 0 },
+      Post_id: { type: Schema.Types.ObjectId, ref: "Post" },
+      Creator: { type: Schema.Types.ObjectId, ref: "users" },
+    },
+  ],
 });
 
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 module.exports =mongoose.model("comment", comment);;
 =======
 module.exports = mongoose.model("comments", Comment);
 >>>>>>> 82223093b13f008567710c889ae1bc1caefff2c8
+=======
+module.exports = mongoose.model("comments", Comment);
+>>>>>>> Stashed changes
